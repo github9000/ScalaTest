d6a3814abc1a3377c7cb7e1cb6c5313da859fb59
Ken Williams <ken.williams@draysontechnologies.com>
2017-11-07 14:03:04 +0000

    Added gui3.scala

---
+++ Tests/gui3.scala
@@ -0,0 +1,27 @@
+
+/*
+    http://otfried.org/scala/index_30.html
+
+    USAGE:
+    > fsc gui3.scala 
+    > scala GuiProgramThree
+    
+*/
+import scala.swing._
+
+class UI extends MainFrame {
+  title = "GUI Program #3"
+  contents = new BoxPanel(Orientation.Vertical) {
+    contents += new Label("Look at me!")
+    contents += Button("Press me, please") { println("Thank you") }
+    contents += Button("Close") { sys.exit(0) }
+  }
+}
+
+object GuiProgramThree {
+  def main(args: Array[String]) {
+    val ui = new UI
+    ui.visible = true
+  }
+}
+
